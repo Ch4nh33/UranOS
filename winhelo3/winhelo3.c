b8f9bef3bc6f41d0ae40905b8ca089e74a4c86b4
Chan Hee <78791504+Ch4nh33@users.noreply.github.com>
2024-07-03 02:41:49 +0900

    Update winhelo3.c

--- winhelo3/winhelo3.c
+++ winhelo3/winhelo3.c
@@ -8,11 +8,11 @@ void HariMain(void)
 	api_initmalloc();
 	buf = api_malloc(150 * 50);
 	win = api_openwin(buf, 150, 50, -1, "hello");
-	api_boxfilwin(win,  8, 36, 141, 43, 6 /* 물색 */);
-	api_putstrwin(win, 28, 28, 0 /* 검정 */, 12, "hello, world");
+	api_boxfilwin(win,  8, 36, 141, 43, 6 );
+	api_putstrwin(win, 28, 28, 0 , 12, "hello, world");
 	for (;;) {
 		if (api_getkey(1) == 0x0a) {
-			break; /* Enter라면 break; */
+			break; /* Enter쨍 break; */
 		}
 	}
 	api_end();
